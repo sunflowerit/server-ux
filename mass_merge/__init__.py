51e9db8925ceb175e8d82fe65c5c8e3cf0410d18
Kiplangat Dan <kiplangatdan@gmail.com>
2022-03-11 14:40:46 +0300

    [ADD] mass_merge

---
+++ mass_merge/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+#  OpenERP, Open Source Management Solution
+#  Copyright (C) 2012 Serpent Consulting Services (<http://www.serpentcs.com>)
+#  Copyright (C) 2010-Today OpenERP SA (<http://www.openerp.com>)
+#  Copyright (c) 2019-Today Sunflower IT (<https://www.sunflowerweb.nl>)
+#  License GNU General Public License see <http://www.gnu.org/licenses/>
+
+from . import models
+from . import tools
+from . import wizard
