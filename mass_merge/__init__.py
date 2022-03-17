7f0e8a1ae1871baf8787b3e5d397cebc59dc469d
douglas-tabut <tabutdouglas@gmail.com>
2022-03-17 13:57:09 +0300

    [IMP] bring merge_editing_product into mass_merge

--- mass_merge/__init__.py
+++ mass_merge/__init__.py
@@ -8,3 +8,4 @@
 from . import models
 from . import tools
 from . import wizard
+from .hooks import post_init_hook
--- mass_merge/__manifest__.py
+++ mass_merge/__manifest__.py
@@ -23,4 +23,5 @@
     ],
     'installable': True,
     'application': True,
+    'post_init_hook': 'post_init_hook',
 }
