0be31a3769221c82cde9823cd973a9f02799c6b5
douglas-tabut <tabutdouglas@gmail.com>
2022-04-06 11:22:33 +0300

    [ADD]Fix value error for ref field to merge records

--- mass_merge/models/merge_editing.py
+++ mass_merge/models/merge_editing.py
@@ -15,17 +15,6 @@ class MergeObject(models.Model):
         help="Sidebar action to make this template\
         available on records of the related document\
         model")
-    ref_ir_value_fuse = fields.Many2one(
-        'ir.actions.act_window', 'Sidebar fuse button',
-        readonly=True, help="Sidebar button to\
-        open the sidebar action")
-    ref_ir_act_window_fuse = fields.Many2one(
-        "ir.actions.act_window",
-        "Sidebar fuse button",
-        readonly=True,
-        help="Sidebar button to\
-        open the sidebar action",
-    )
     model_list = fields.Char('Model List', size=256)
 
     @api.onchange('model_id')
@@ -70,10 +59,6 @@ class MergeObject(models.Model):
                 if template.ref_ir_act_server_fuse:
                     self.env['ir.actions.server'].search(
                         [('id', '=', template.ref_ir_act_server_fuse.id)]).unlink()
-                if template.ref_ir_act_window_fuse:
-                    ir_values_obj = self.env['ir.actions.act_window']
-                    ir_values_obj.search(
-                        [('id', '=', template.ref_ir_act_window_fuse.id)]).unlink()
             except:
                 raise UserError(_("Deletion of the action record failed."))
         return True
