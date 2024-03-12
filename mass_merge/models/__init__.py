36b4db43160364de69bf9b26be7dd2acef490c2f
ntsirintanis <ntsirintanis@therp.nl>
2024-03-12 15:37:50 +0100

    [ADD]Technical feature to merge records from any table (feature from module=record_merge)

--- mass_merge/models/__init__.py
+++ mass_merge/models/__init__.py
@@ -2,6 +2,7 @@
 
 from . import merge_editing
 <<<<<<< HEAD
+<<<<<<< HEAD
 from . import record_merge_mixin
 from . import record_merge_id
 from . import record_merge_criteria
@@ -11,3 +12,9 @@ from . import ir_ui_view
 
 =======
 >>>>>>> ed3987d6 ([ADD] mass_merge)
+=======
+from . import record_merge_mixin
+from . import record_merge_id
+from . import record_merge_criteria
+
+>>>>>>> 3833506c ([ADD]Technical feature to merge records from any table (feature from module=record_merge))
